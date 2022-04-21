401bd5e17c3c5f59e733e99cae5cf500a257717f
Ekim Tuna Öztürk <enistoprakozturk@gmail.com>
2022-04-21 16:36:29 +0300

    for db creation

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from distutils.log import debug
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
 from src import *
 import os
 
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+db = SQLAlchemy(app)
+api = Api(app)
 
 if __name__ == "__main__":
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
     app.run(debug=True)
